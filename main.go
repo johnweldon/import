13bf3cf4d3cc02b6172d872ce1594c393c43bd09
John Weldon <johnweldon4@gmail.com>
2017-11-11 13:26:29 -0700

    Add jw4.us/location

--- main.go
+++ main.go
@@ -42,10 +42,11 @@ var (
   </body>
 </html>`))
 	imports = map[string]*data{
-		"jw4.us/import":  &data{ImportRoot: "jw4.us/import", VCS: "git", VCSRoot: "https://github.com/johnweldon/import", Suffix: ""},
-		"jw4.us/mqd":     &data{ImportRoot: "jw4.us/mqd", VCS: "git", VCSRoot: "https://github.com/jw4/mqd", Suffix: ""},
-		"jw4.us/tunnel":  &data{ImportRoot: "jw4.us/tunnel", VCS: "git", VCSRoot: "https://github.com/johnweldon/tunnel", Suffix: ""},
-		"jw4.us/sortcsv": &data{ImportRoot: "jw4.us/sortcsv", VCS: "git", VCSRoot: "https://github.com/johnweldon/sortcsv", Suffix: ""},
+		"jw4.us/import":   &data{ImportRoot: "jw4.us/import", VCS: "git", VCSRoot: "https://github.com/johnweldon/import", Suffix: ""},
+		"jw4.us/mqd":      &data{ImportRoot: "jw4.us/mqd", VCS: "git", VCSRoot: "https://github.com/jw4/mqd", Suffix: ""},
+		"jw4.us/tunnel":   &data{ImportRoot: "jw4.us/tunnel", VCS: "git", VCSRoot: "https://github.com/johnweldon/tunnel", Suffix: ""},
+		"jw4.us/sortcsv":  &data{ImportRoot: "jw4.us/sortcsv", VCS: "git", VCSRoot: "https://github.com/johnweldon/sortcsv", Suffix: ""},
+		"jw4.us/location": &data{ImportRoot: "jw4.us/location", VCS: "git", VCSRoot: "https://github.com/johnweldon/location-service", Suffix: ""},
 	}
 )
 
